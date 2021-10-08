b04a89c4ca1e7b57fe07ffd6a63dbc1ea86a3a0b
CaedenPH <caedenperelliharris@gmail.com>
2021-10-09 00:03:12 +0100

    push

--- script.js
+++ script.js
@@ -35,3 +35,46 @@ var aText = new Array(
     
     
     typewriter();
+
+const list = document.querySelector('.list');
+const skills = new Map([
+    [' Python', 'fa-python'],
+    [' Bash', 'fa-codepen'],
+    [' HTML', 'fa-html5'],
+    [' CSS', 'fa-css3'],
+    [' Javascript', 'fa-js'],
+    [' NodeJS', 'fa-node-js'],
+    [' Java', 'fa-java'],
+    [' MongoDB', 'fa-codepen'],
+    [' PostgreSQL', 'fa-codepen'],
+    [' Cloud', 'fa-codepen'],
+    [' Linux', 'fa-linux'],
+    [' Windows', 'fa-windows'],
+    [' Replit', 'fa-codepen'],
+    [' Visual Studio Code', 'fa-codepen'],
+    [' GitHub', 'fa-github'],
+    [' Discord', 'fa-discord']
+]);
+
+for (let [k, v] of skills.entries()) {
+    const li = document.createElement('li');
+    const i = document.createElement('i');
+    const t = document.createTextNode(k);
+    ['fab', v].forEach(c => i.classList.add(c));
+    li.append(i);
+    li.append(t);
+    list.append(li);
+}
+
+function changeText(x) {
+    if (x.matches) {
+        document.getElementById("footText").innerHTML = "Caeden";
+    } else {
+        document.getElementById("footText").innerHTML = "© 2021- Caeden Perelli-Harris";
+    }
+  }
+  
+var x = window.matchMedia("(max-width: 640px)")
+changeText(x)
+window.onresize = changeText
+
